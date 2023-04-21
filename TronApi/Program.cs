48be77eb982ba135e9ee4f383a3032eda18d1601
Vincento1111 <vincento2009@hotmail.com>
2023-04-21 03:19:38 +0200

    removed if dev

--- TronApi/Program.cs
+++ TronApi/Program.cs
@@ -33,16 +33,16 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "swagger"; // Serve Swagger UI under the /swagger path
-    });
 
-}
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.RoutePrefix = "swagger"; // Serve Swagger UI under the /swagger path
+        });
+
+
 
 app.UseHttpsRedirection();
 
