0497a8f1d51eda62b1dbf7029c0201dfa4afb60a
Vincento1111 <vincento2009@hotmail.com>
2023-04-23 00:49:37 +0200

    undid migration i think

--- TronApi/Migrations/20230422224849_removedprevious.Designer.cs
+++ TronApi/Migrations/20230422224849_removedprevious.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TronApi.Data;
 
@@ -10,9 +11,10 @@ using TronApi.Data;
 namespace TronApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230422224849_removedprevious")]
+    partial class removedprevious
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TronApi/Migrations/20230422224849_removedprevious.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TronApi.Migrations
+{
+    public partial class removedprevious : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
