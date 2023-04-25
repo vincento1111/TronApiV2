fa504ad732f289d5e2e62810b7bdf42f9b2ee611
Vincento1111 <vincento2009@hotmail.com>
2023-04-25 03:46:20 +0200

    updated training controller

--- TronApi/Controllers/UserStatsController.cs
+++ TronApi/Controllers/UserStatsController.cs
@@ -39,48 +39,81 @@ namespace TronApi.Controllers
             return Ok(await _context.UsersStats.ToListAsync());
         }
 
-        [HttpPut]
-
-        public async Task<ActionResult<List<UserStats>>> UpdateStats(UserStats Request)
+        [HttpPut("{userId}/{statType}")]
+        public async Task<ActionResult<UserStats>> UpdateStats(int userId, int statType)
         {
-            var dbStats = await _context.UsersStats.FindAsync(Request.StatId);
+            var dbStats = await _context.UsersStats.FirstOrDefaultAsync(u => u.UserId == userId);
             if (dbStats == null)
                 return NotFound("stats not found");
-            if(Request.Strength != 0)
+
+            switch (statType)
             {
-                dbStats.Strength = Request.Strength;
-            }if(Request.Defense != 0)
-            {
-                dbStats.Defense = Request.Defense;
+                case 1:
+                    dbStats.Strength++;
+                    break;
+                case 2:
+                    dbStats.Speed++;
+                    break;
+                case 3:
+                    dbStats.Defense++;
+                    break;
+                case 4:
+                    dbStats.Dexterity++;
+                    break;
+                default:
+                    return BadRequest("Invalid stat type");
             }
-            if(Request.Speed != 0)
-            {
-                dbStats.Speed = Request.Speed;
-            }
-            if (Request.Dexterity != 0)
-            {
-                dbStats.Dexterity = Request.Dexterity;
-            }
-            if (Request.Experience != 0)
-            {
-                dbStats.Experience = Request.Experience;
-            }
-            if (Request.Life != 0)
-            {
-                dbStats.Life = Request.Life;
-            }
-            if (Request.Money != 0)
-            {
-                dbStats.Money = Request.Money;
-            }
-            if (Request.Level != 0)
-            {
-                dbStats.Level = Request.Level;
-            }
+
             await _context.SaveChangesAsync();
 
-            return Ok(await _context.UsersStats.ToListAsync());
+            return Ok(dbStats);
         }
+
+
+
+
+        //[HttpPut]
+
+        //public async Task<ActionResult<List<UserStats>>> UpdateStats(UserStats Request)
+        //{
+        //    var dbStats = await _context.UsersStats.FindAsync(Request.StatId);
+        //    if (dbStats == null)
+        //        return NotFound("stats not found");
+        //    if(Request.Strength != 0)
+        //    {
+        //        dbStats.Strength = Request.Strength;
+        //    }if(Request.Defense != 0)
+        //    {
+        //        dbStats.Defense = Request.Defense;
+        //    }
+        //    if(Request.Speed != 0)
+        //    {
+        //        dbStats.Speed = Request.Speed;
+        //    }
+        //    if (Request.Dexterity != 0)
+        //    {
+        //        dbStats.Dexterity = Request.Dexterity;
+        //    }
+        //    if (Request.Experience != 0)
+        //    {
+        //        dbStats.Experience = Request.Experience;
+        //    }
+        //    if (Request.Life != 0)
+        //    {
+        //        dbStats.Life = Request.Life;
+        //    }
+        //    if (Request.Money != 0)
+        //    {
+        //        dbStats.Money = Request.Money;
+        //    }
+        //    if (Request.Level != 0)
+        //    {
+        //        dbStats.Level = Request.Level;
+        //    }
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok(await _context.UsersStats.ToListAsync());
+        //}
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<UserStats>>> Delete(int id)
