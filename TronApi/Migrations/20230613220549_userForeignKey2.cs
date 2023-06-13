e5774dcca639ea580c9e4c034e36eeb8d3f14339
Vincento1111 <vincento2009@hotmail.com>
2023-06-14 01:50:37 +0200

    update

---
+++ TronApi/Migrations/20230613220549_userForeignKey2.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TronApi.Migrations
+{
+    public partial class userForeignKey2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersStats_UserId",
+                table: "UsersStats",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInventories_ItemId",
+                table: "UserInventories",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInventories_UserId",
+                table: "UserInventories",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_Users_UserId",
+                table: "Profiles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserInventories_MasterItemsTables_ItemId",
+                table: "UserInventories",
+                column: "ItemId",
+                principalTable: "MasterItemsTables",
+                principalColumn: "ItemId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserInventories_Users_UserId",
+                table: "UserInventories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersStats_Users_UserId",
+                table: "UsersStats",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_Users_UserId",
+                table: "Profiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserInventories_MasterItemsTables_ItemId",
+                table: "UserInventories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserInventories_Users_UserId",
+                table: "UserInventories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersStats_Users_UserId",
+                table: "UsersStats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UsersStats_UserId",
+                table: "UsersStats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserInventories_ItemId",
+                table: "UserInventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserInventories_UserId",
+                table: "UserInventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles");
+        }
+    }
+}
--- TronApi/Tables/Profile.cs
+++ TronApi/Tables/Profile.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TronApi
 {
     public class Profile
     {
+        [Key]
         public int ProfileId { get; set; }
         [ForeignKey("User")]
         public int UserId { get; set; }
+        public virtual User User { get; set; }
+
         public string ProfileDes { get; set; } = string.Empty;
         public Profile(int userId)
         {
